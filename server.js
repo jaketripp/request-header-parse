09e9ba322011ba2bba3636a5151e204b70754b91
Jake Tripp <shmogens@gmail.com>
2017-11-12 18:33:10 -0600

    add ejs, add css

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
 	var lang = req.headers["accept-language"].split(',')[0];
 	var ip = req.headers['x-forwarded-for'] || 
@@ -9,12 +12,13 @@ app.get('/', (req, res) => {
      (req.connection.socket ? req.connection.socket.remoteAddress : null);
   var regexp = /\(([^)]+)\)/;
   var os = regexp.exec(req.headers['user-agent'])[1];
-  
-	res.send({
-		ipaddress: ip,
-		language: lang,
-		software: os
-	});
+	res.render('index.ejs', {user: 
+                           {
+                              ipaddress: ip,
+                              language: lang,
+                              software: os
+                            }
+  });
 });
 
 const port = process.env.PORT || 3000;
